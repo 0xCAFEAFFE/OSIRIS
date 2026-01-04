c5675d3636cadb105d2c1b9bdf0303f7225b6026
0xCAFEAFFE <105112757+0xCAFEAFFE@users.noreply.github.com>
2026-01-04 18:43:24 +0100

    Fix hardcoded COM port number

--- firmware/application/flash_application.bat
+++ firmware/application/flash_application.bat
@@ -1,3 +1,5 @@
 @echo off
-avrdude -v -V -c arduino -p atmega328pb -P COM7 -b 28800 -D -U flash:w:"./OSIRIS_FW.hex":i 
+echo Enter serial port number, e.g. COM7:
+set /p comport=
+avrdude -v -V -c arduino -p atmega328pb -P %comport% -b 28800 -D -U flash:w:"./OSIRIS_FW.hex":i 
 pause
